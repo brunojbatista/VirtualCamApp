723319773467cda1156e7d4c0801fae1ca174511
Bruno Batista <bjnb.projetos@gmail.com>
2025-05-25 18:27:51 -0300

    feat: add login and check user logged

--- app/src/main/java/com/brunojbatista/virtualcamapp/MainActivity.kt
+++ app/src/main/java/com/brunojbatista/virtualcamapp/MainActivity.kt
@@ -5,17 +5,52 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Inicialização do App Check do Firebase
+        FirebaseApp.initializeApp(this)
+        //val factory = PlayIntegrityAppCheckProviderFactory.getInstance()
+        val factory = DebugAppCheckProviderFactory.getInstance()
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(factory)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Espera 2 segundos, depois abre a tela de login
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish() // finaliza a splash
-        }, 2000) // 2000ms = 2 segundos
+        firebaseAuth.signOut()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var intent: Intent? = null
+        if (firebaseAuth.currentUser != null) {
+            // Fazer uma verificação se tem algum plano já existente
+            // para mandar para a tela principal do app
+            // caso não tenha manda para a tela de pagamentos
+
+
+            // Ir para a tela de pagamento por padrão, caso usuário logado não tenha plano
+            intent = Intent(applicationContext, PaymentActivity::class.java)
+
+            // Verificar se tem algum plano ativo
+            // intent = Intent(applicationContext, PaymentActivity::class.java)
+        } else {
+            // Caso usuário deslogado, mande para a tela de login
+            intent = Intent(applicationContext, LoginActivity::class.java)
+        }
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
